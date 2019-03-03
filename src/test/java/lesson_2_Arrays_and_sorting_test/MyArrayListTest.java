feb8f5a3f18cd8a000f62787d0b23abaf0d9d274
Antony.Glim <Antony.Glim@gmail.com>
2019-03-03 12:10:04 +0300

    JUnit tests for list add

--- src/test/java/lesson_2_Arrays_and_sorting_test/MyArrayListTest.java
+++ src/test/java/lesson_2_Arrays_and_sorting_test/MyArrayListTest.java
@@ -1,4 +1,27 @@
 package lesson_2_Arrays_and_sorting_test;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
 public class MyArrayListTest {
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {new int[]{}, false},
+                {new int[]{1, 1, 1, 1, 1, 1, 1}, false},
+                {new int[]{4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4}, false},
+                {new int[]{5, 5, 5, 5, 5, 5, 5, 5, 5, 5}, false},
+                {new int[]{1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4}, true},
+                {new int[]{1, 4}, true},
+                {new int[]{1}, false},
+                {new int[]{4}, false},
+        });
+    }
+    private int[] arrStart;
+    private boolean expectedResult;
 }

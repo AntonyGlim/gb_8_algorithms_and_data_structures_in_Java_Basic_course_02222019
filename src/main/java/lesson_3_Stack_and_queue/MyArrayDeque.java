f109c1ad8d049ca82aca8794d7bfebb94cbc5440
Antony.Glim <Antony.Glim@gmail.com>
2019-03-05 21:26:31 +0300

    MyArrayDeque processing 30

--- src/main/java/lesson_3_Stack_and_queue/MyArrayDeque.java
+++ src/main/java/lesson_3_Stack_and_queue/MyArrayDeque.java
@@ -1,4 +1,104 @@
 package lesson_3_Stack_and_queue;
 
-public class MyArrayDeque {
+import java.util.NoSuchElementException;
+
+public class MyArrayDeque <Item> {
+
+    Object[] deque = new Object[1];
+    int size = 0;
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    /**
+     * Изменить размер дэка
+     * @param capacity
+     */
+    public void resize(int capacity){
+        Object[] temp = new Object[capacity];
+        for (int i = 0; i < size; i++) {
+            temp[i] = deque[i];
+        }
+        deque = temp;
+    }
+
+    /**
+     * Удалить все элементы из дэка
+     */
+    public void clear(){
+        Object[] temp = new Object[1];
+        deque = temp;
+        size = 0;
+    }
+
+    /**
+     * Добавление элемента в конец дэка
+     * @param item
+     */
+    public void pushBack(Item item){
+        if (size == deque.length){
+            resize(deque.length * 2);
+        }
+        deque[size++] = item;
+    }
+
+    /**
+     * Просмотреть элемент в конце дэка
+     * @return
+     */
+    public Item peekBack(){
+        if (isEmpty()){
+            throw new NoSuchElementException();
+        }
+        return (Item) deque[size - 1];
+    }
+
+    /**
+     * Извлечение элемента из конеца дэка
+     * @return
+     */
+    public Item popBack(){
+        if (isEmpty()){
+            throw new NoSuchElementException();
+        }
+        Item item = (Item) deque[size - 1];
+        size--;
+        if (size == deque.length / 4 && size > 0){
+            resize (deque.length / 2);
+        }
+        return item;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < size; i++) {
+            stringBuilder.append((Item)deque[i].toString()).append(", ");
+        }
+        return stringBuilder.toString();
+    }
+
+    //push_front
+    //Добавить (положить) в начало дека новый элемент
+    //push_back
+    //Добавить (положить) в конец дека новый элемент
+    //pop_front
+    //Извлечь из дека первый элемент
+    //pop_back
+    //Извлечь из дека последний элемент
+    //front
+    //Узнать значение первого элемента (не удаляя его)
+    //back
+    //Узнать значение последнего элемента (не удаляя его)
+    //size
+    //Узнать количество элементов в деке
+    //clear
+    //Очистить дек (удалить из него все элементы)
+
+
 }
--- src/test/java/lesson_3_Stack_and_queue_test/MyArrayDequeTest.java
+++ src/test/java/lesson_3_Stack_and_queue_test/MyArrayDequeTest.java
@@ -1,4 +1,64 @@
 package lesson_3_Stack_and_queue_test;
 
+import lesson_3_Stack_and_queue.MyArrayDeque;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
 public class MyArrayDequeTest {
+
+    static int count = 0;
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {new char[]{'a', 'b', 'c', 'd', 'e', 'z'}, 6, new char[]{'d', 'e', 'z'} },
+                {new char[]{'z', 'x', 'c', 'y', 'e'}, 5, new char[]{'y', 'e'} },
+                {new char[]{'a', 'b', 'c', 'd', 'r', 'f', 'e'}, 7, new char[]{'d', 'r', 'f', 'e'} },
+                {new char[]{'a', 'b', 'c', 'd'}, 4, new char[]{'d'} },
+                {new char[]{}, 0, new char[]{}},
+        });
+    }
+
+    private char[] symbolsBefore;
+    private int DequeSize;
+    private char[] symbolsAfter;
+
+    public MyArrayDequeTest(char[] symbolsBefore, int dequeSize, char[] symbolsAfter) {
+        this.symbolsBefore = symbolsBefore;
+        DequeSize = dequeSize;
+        this.symbolsAfter = symbolsAfter;
+    }
+
+    MyArrayDeque<Character> deque;
+
+    @Before
+    public void init(){
+        deque = new MyArrayDeque<Character>();
+        System.out.println("Test #" + ++count);
+    }
+
+    @Test
+    public void test1(){
+
+        //
+        for (int i = 0; i < symbolsBefore.length; i++) {
+            deque.pushBack(symbolsBefore[i]);
+            System.out.println("pushBack() : " + (char) symbolsBefore[i]);
+        }
+        System.out.println("Before start testing: " + deque + "(size = " + deque.size() + ")");
+    }
+
+    @After
+    public void result(){
+        System.out.println("After start testing: " + deque + "(size = " + deque.size() + ")" + "\n");
+    }
+
+
 }

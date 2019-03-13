c692493fbf9028db7327397314a62578b6ee1f15
Antony.Glim <Antony.Glim@gmail.com>
2019-03-13 23:04:20 +0300

    MySumOfDigits re factored

--- src/main/java/lesson_5_Recursion/MySumOfDigits.java
+++ src/main/java/lesson_5_Recursion/MySumOfDigits.java
@@ -1,16 +1,21 @@
 package lesson_5_Recursion;
 
+/**
+ * Найти сумму цифр числа
+ * f(n) - сумма чисел
+ * f(n) = f(n/10) + n % 10;
+ * f(5845) = (5 + 8 + 4) + 5 = f(584) + 5
+ */
 public class MySumOfDigits {
 
-    //Найти сумму цифр числа
-    //f(n) - умма чисел
-    //f(n) = f(n/10) + n % 10;
-    //f(5845) = (5 + 8 + 4) + 5 = f(584) + 5
     public static void main(String[] args) {
         System.out.println(symbolsSum(5845));
         System.out.println(symbolsSumRec(5845));
     }
 
+    /**
+     *  Вычисляем при помощи циклов
+     */
     public static int symbolsSum(int n){
         int sum = 0;
         while (n > 0){
@@ -21,9 +26,12 @@ public class MySumOfDigits {
         return sum;
     }
 
+    /**
+     *  Вычисляем при помощи рекурсии
+     */
     public static int symbolsSumRec(int n){
         if (n < 10) return n;
         else return symbolsSumRec(n / 10) + n % 10;
-
     }
+
 }

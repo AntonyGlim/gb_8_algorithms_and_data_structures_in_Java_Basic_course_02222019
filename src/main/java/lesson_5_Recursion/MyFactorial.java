25df112a6b1c9193cddc76440e78e4586ca3c017
Antony.Glim@gmail.com <Antony.Glim@gmail.com>
2019-03-13 10:23:08 +0300

    MyFactorial done!

--- src/main/java/lesson_5_Recursion/MyFactorial.java
+++ src/main/java/lesson_5_Recursion/MyFactorial.java
@@ -1,20 +1,17 @@
 package lesson_5_Recursion;
 
+/**
+ * Дано натуральное число n, найти n! = (1 * 2 * ... * [n - 1]) * n = (n - 1)! * n
+ * f(n) = f(n - 1) * n;
+ */
 public class MyFactorial {
-
-    //Дано натуральное число n, найти n! = (1 * 2 * ... * [n - 1]) * n = (n - 1)! * n
-    //f(n) = f(n - 1) * n;
-    public static void main(String[] args) {
-        System.out.println(fact(5));
-        System.out.println(factRec(5));
-    }
 
     /**
      * Вычисление факториала (циклическим способом)
      * @param n - натуральное число
      * @return - факториал n
      */
-    public static long fact(int n){
+    public long fact(int n){
         long mult = 1;
         while (n > 0) mult *= n--;
         return mult;
@@ -26,8 +23,9 @@ public class MyFactorial {
      * @param n - натуральное число
      * @return - факториал n
      */
-    public static long factRec(int n){
+    public long factRec(int n){
         if (n < 2) return 1;
         else return factRec(n - 1) * n;
     }
+
 }

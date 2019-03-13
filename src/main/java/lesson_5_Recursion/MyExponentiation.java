5c4ebf2837815583bbdc62ceacf1fc3eeb38b560
Antony.Glim <Antony.Glim@gmail.com>
2019-03-13 23:49:51 +0300

    MyExponentiation done!

--- src/main/java/lesson_5_Recursion/MyExponentiation.java
+++ src/main/java/lesson_5_Recursion/MyExponentiation.java
@@ -1,4 +1,28 @@
 package lesson_5_Recursion;
 
+/**
+ * Дано натуральное число a, которое необходимо возвести в степень n.
+ * f(a, n) = a * a * ... * a = (a^[n - 1]) * a;
+ */
 public class MyExponentiation {
+
+    /**
+     *  Вычисляем при помощи циклов
+     */
+    public long expo(int a, int n){
+        long exp = 1;
+        for (int i = 0; i < n; i++) {
+            exp *= a;
+        }
+        return exp;
+    }
+
+    /**
+     *  Вычисляем при помощи рекурсии
+     */
+    public long expoRec(int a, int n){
+        if (n == 1) return a;
+        if (n < 1) return 1;
+        else return expoRec(a, n - 1) * a;
+    }
 }

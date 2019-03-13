f24b28c9f72c967fa1827dc85ee48408c4b4b30f
Antony.Glim <Antony.Glim@gmail.com>
2019-03-13 23:50:33 +0300

    MySumOfDigits changed

--- src/main/java/lesson_5_Recursion/MySumOfDigits.java
+++ src/main/java/lesson_5_Recursion/MySumOfDigits.java
@@ -8,17 +8,6 @@ package lesson_5_Recursion;
  */
 public class MySumOfDigits {
 
-    public static void main(String[] args) {
-        System.out.println(symbolsSum(0));
-        System.out.println(symbolsSum(1));
-        System.out.println(symbolsSum(2));
-        System.out.println(symbolsSum(10));
-        System.out.println(symbolsSum(5795));
-        System.out.println(symbolsSum(584579));
-        System.out.println(symbolsSum(584565461));
-        System.out.println(symbolsSum(584599999));
-    }
-
     /**
      *  Вычисляем при помощи циклов
      */

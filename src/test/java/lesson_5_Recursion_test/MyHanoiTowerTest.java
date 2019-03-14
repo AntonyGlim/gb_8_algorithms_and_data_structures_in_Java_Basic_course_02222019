d6ddce98933dac172c873cb32862de464015a4cc
Antony.Glim <Antony.Glim@gmail.com>
2019-03-14 23:11:26 +0300

    MyHanoiTowerTest todo add

--- src/test/java/lesson_5_Recursion_test/MyHanoiTowerTest.java
+++ src/test/java/lesson_5_Recursion_test/MyHanoiTowerTest.java
@@ -15,46 +15,47 @@ import java.util.Collection;
  * Проверка корректности вычисляемых значений.
  * Сравнение циклического и рекурсивного способа вычисления.
  * Способы сравниваются по количеству затраченого времени
+ * TODO тесты пока не написаны
  */
 @RunWith(Parameterized.class)
 public class MyHanoiTowerTest {
-
-    static int count = -1;
-    static long timeStart = 0L;
-    static long timeStop = 0L;
-
-    @Parameterized.Parameters
-    public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][]{
-                {},
-        });
-    }
-
-    private int a;
-    private int n;
-    private long result;
-
-    public MyHanoiTowerTest(int a, int n, long result) {
-        this.a = a;
-        this.n = n;
-        this.result = result;
-    }
-
-    MyHanoiTower towerTest;
-
-    @Before
-    public void init() {
-        towerTest = new MyHanoiTower(6);
-        System.out.println("Test #" + ++count);
-    }
-
-    @Test
-    public void test(){
-
-    }
-
-    @After
-    public void result(){
-        System.out.println("Test #" + count + " finished" + "\n");
-    }
+//
+//    static int count = -1;
+//    static long timeStart = 0L;
+//    static long timeStop = 0L;
+//
+//    @Parameterized.Parameters
+//    public static Collection<Object[]> data() {
+//        return Arrays.asList(new Object[][]{
+//                {},
+//        });
+//    }
+//
+//    private int a;
+//    private int n;
+//    private long result;
+//
+//    public MyHanoiTowerTest(int a, int n, long result) {
+//        this.a = a;
+//        this.n = n;
+//        this.result = result;
+//    }
+//
+//    MyHanoiTower towerTest;
+//
+//    @Before
+//    public void init() {
+//        towerTest = new MyHanoiTower(6);
+//        System.out.println("Test #" + ++count);
+//    }
+//
+//    @Test
+//    public void test(){
+//
+//    }
+//
+//    @After
+//    public void result(){
+//        System.out.println("Test #" + count + " finished" + "\n");
+//    }
 }

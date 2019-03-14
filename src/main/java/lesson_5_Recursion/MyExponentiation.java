eb37f5d0b635f89f5ffb7a3b065f4c2cbb20a80d
Antony.Glim@gmail.com <Antony.Glim@gmail.com>
2019-03-14 12:20:42 +0300

    MyExponentiation quick exp add

--- src/main/java/lesson_5_Recursion/MyExponentiation.java
+++ src/main/java/lesson_5_Recursion/MyExponentiation.java
@@ -8,11 +8,28 @@ public class MyExponentiation {
 
     /**
      *  Вычисляем при помощи циклов
+     *  Медленное возведение
      */
     public long expo(int a, int n){
         long exp = 1;
         for (int i = 0; i < n; i++) {
             exp *= a;
+        }
+        return exp;
+    }
+
+    /**
+     *  Вычисляем при помощи циклов
+     *  Быстрое возведение (добавлена некорорая оптимизация)
+     */
+    public long qExpo(int a, int n){
+        long exp = 1;
+        while (n > 0){
+            if (n % 2 == 1){
+                exp *= a;
+            }
+            a *= a;
+            n /= 2;
         }
         return exp;
     }
@@ -24,5 +41,15 @@ public class MyExponentiation {
         if (n == 1) return a;
         if (n < 1) return 1;
         else return expoRec(a, n - 1) * a;
+    }
+
+    /**
+     *  Вычисляем при помощи рекурсии
+     *  Быстрое возведение (добавлена некорорая оптимизация)
+     */
+    public long qExpoRec(int a, int n){
+        if (n == 1) return a;
+        if (n < 1) return 1;
+        else return qExpoRec(a * a, n / 2) * a;
     }
 }

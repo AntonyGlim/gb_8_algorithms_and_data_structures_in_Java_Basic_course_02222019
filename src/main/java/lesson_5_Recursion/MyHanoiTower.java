5088b34008d27f9d10f9758bd456aa82ce191b5d
Antony.Glim <Antony.Glim@gmail.com>
2019-03-14 20:54:29 +0300

    MyHanoiTower simple implementation

--- src/main/java/lesson_5_Recursion/MyHanoiTower.java
+++ src/main/java/lesson_5_Recursion/MyHanoiTower.java
@@ -10,4 +10,20 @@ package lesson_5_Recursion;
  */
 public class MyHanoiTower {
 
+    public void move(char kernel1, char kernel2){
+        System.out.println(String.format("Из стрежня %s на стержень %s", kernel1, kernel2 ));
+    }
+
+    public void moveTower(int n, char kernel1, char kernel2, char kernelTemp){
+        if(n == 0) return;
+        moveTower(n - 1, kernel1, kernelTemp, kernel2);
+        move(kernel1, kernel2);
+        moveTower(n - 1, kernelTemp, kernel2, kernel1);
+    }
+
+    public static void main(String[] args) {
+        MyHanoiTower tower = new MyHanoiTower();
+        tower.moveTower(8, 'A', 'B', 'C');
+    }
+
 }

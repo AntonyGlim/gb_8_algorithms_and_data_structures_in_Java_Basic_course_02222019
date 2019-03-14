84a7a997082412fa0bbfcbb8e612e09019886e1e
Antony.Glim@gmail.com <Antony.Glim@gmail.com>
2019-03-14 12:51:44 +0300

    MyHanoiTowerTest created

---
+++ src/test/java/lesson_5_Recursion_test/MyHanoiTowerTest.java
@@ -0,0 +1,62 @@
+package lesson_5_Recursion_test;
+
+import lesson_5_Recursion.MyExponentiation;
+import lesson_5_Recursion.MyHanoiTower;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ * Тестирование класса MyHanoiTower
+ * Проверка корректности вычисляемых значений.
+ * Сравнение циклического и рекурсивного способа вычисления.
+ * Способы сравниваются по количеству затраченого времени
+ */
+@RunWith(Parameterized.class)
+public class MyHanoiTowerTest {
+
+    static int count = -1;
+    static long timeStart = 0L;
+    static long timeStop = 0L;
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {},
+        });
+    }
+
+    private int a;
+    private int n;
+    private long result;
+
+    public MyHanoiTowerTest(int a, int n, long result) {
+        this.a = a;
+        this.n = n;
+        this.result = result;
+    }
+
+    MyHanoiTower towerTest;
+
+    @Before
+    public void init() {
+        towerTest = new MyHanoiTower();
+        System.out.println("Test #" + ++count);
+    }
+
+    @Test
+    public void test(){
+
+    }
+
+    @After
+    public void result(){
+        System.out.println("Test #" + count + " finished" + "\n");
+    }
+}

8485c3b13b227d04ef7175ec79d4d404f3703ba6
Antony.Glim <Antony.Glim@gmail.com>
2019-03-22 21:25:41 +0300

    MyChainingHashMap methods add

--- src/main/java/lesson_8_Hash_tables/MyChainingHashMap.java
+++ src/main/java/lesson_8_Hash_tables/MyChainingHashMap.java
@@ -1,4 +1,72 @@
 package lesson_8_Hash_tables;
 
-public class MyChainingHashMap {
+public class MyChainingHashMap<Key, Value> {
+    private final int M = 97;
+    private int size = 0;
+    private Object[] st = new Object[M];
+
+    private class Node{
+        Key key;
+        Value value;
+        Node next;
+
+        public Node(Key key, Value value, Node next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    /**
+     * Ф-ыя найдет нужный нам хэш
+     */
+    public int hash(Key key){
+        return (key.hashCode() & 0x7fffffff) % M;
+    }
+
+    public Value get(Key key){
+        if (key == null){
+            throw new IllegalArgumentException("");
+        }
+        int i = hash(key);
+        Node x = (Node) st[i];
+        while (x != null){
+            if (key.equals((x.key))){
+                return x.value;
+            }
+            x = x.next;
+        }
+        return null;
+    }
+
+    public boolean contains(Key key){
+        return get(key) != null;
+    }
+
+    public void put(Key key, Value value){
+        if (key == null){
+            throw new IllegalArgumentException("Ключ не может быть null");
+        }
+        int i = hash(key);
+        Node x = (Node) st[i];
+        while (x != null){
+            if (key.equals((x.key))){
+                x.value = value;
+                return;
+            }
+            x = x.next;
+        }
+        st[i] = new Node(key, value, (Node) st[i]);
+        size++;
+    }
+
+    //Метод ремув в ДЗ
 }
